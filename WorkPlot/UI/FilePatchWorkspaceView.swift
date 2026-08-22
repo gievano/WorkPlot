8a9ab4c7a48307c0c0c224f1771e8ded2fd84060
Arliezv <rigiemessarevano@gmail.com>
2026-08-22 20:59:57 +0700

    fix: reuse shared activity sheet

--- WorkPlot/UI/FilePatchWorkspaceView.swift
+++ WorkPlot/UI/FilePatchWorkspaceView.swift
@@ -595,16 +595,6 @@ struct FilePatchWorkspaceView: View {
     }
 }
 
-private struct ActivityShareSheet: UIViewControllerRepresentable {
-    let items: [Any]
-
-    func makeUIViewController(context: Context) -> UIActivityViewController {
-        UIActivityViewController(activityItems: items, applicationActivities: nil)
-    }
-
-    func updateUIViewController(_ controller: UIActivityViewController, context: Context) {}
-}
-
 // MARK: - Row
 
 struct FileBrowserRowView: View {
